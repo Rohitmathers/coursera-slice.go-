7e2b1e00ed37e62e6b0d6a55845c21b29dea53b6
Rohitmathers <55293244+Rohitmathers@users.noreply.github.com>
2020-04-11 15:21:17 +0530

    Update slice.go

--- slice.go
+++ slice.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"unicode"
 )
 
 func main() {
-	i := 0
-	s := make([]int64, 3)
+	s := make([]int, 3)
+	s1 := make([]int, 0)
 	for {
 		var str string
 		fmt.Println("enter a number or x to exit")
@@ -17,24 +18,69 @@ func main() {
 		if err == nil {
 			isint := isInt(str)
 			if isint { //checking if integer or not
-				var num int64
-				num, err1 := strconv.ParseInt(str, 10, 64) //converting string to integer
+				var num int
+				num, err1 := strconv.Atoi(str) //converting string to integer
 				if err1 == nil {
-					if i < 3 {
-						s[i] = num
-						for temp := 0; temp <= i; temp++ {
-							fmt.Printf("%d ", s[temp])
-						}
-					} else {
-						s = append(s, num)
-						for temp := 0; temp <= i; temp++ {
-							fmt.Printf("%d ", s[temp])
-						}
-					}
-					i++
+					s1 = append(s1, num)
 					fmt.Println("")
+					sort.Ints(s1)
 				}
+				fmt.Println(s1)
+				s = s1
 			} else {
+				fmt.Println(s)
+				if str == "x" { //checking if user wants to exit or not
+					break
+				} else {
+					fmt.Println("Invalid Input")
+					continue
+				}
+
+			}
+		}
+	}
+}
+
+//Function to check whether the string is numeric or not.
+func isInt(s string) bool {
+	for _, c := range s {
+		if !unicode.IsDigit(c) {
+			return false
+		}
+	}
+	return true
+}
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"unicode"
+)
+
+func main() {
+	s := make([]int, 3)
+	s1 := make([]int, 0)
+	for {
+		var str string
+		fmt.Println("enter a number or x to exit")
+		_, err := fmt.Scanln(&str)
+		//user input
+		if err == nil {
+			isint := isInt(str)
+			if isint { //checking if integer or not
+				var num int
+				num, err1 := strconv.Atoi(str) //converting string to integer
+				if err1 == nil {
+					s1 = append(s1, num)
+					fmt.Println("")
+					sort.Ints(s1)
+				}
+				fmt.Println(s1)
+				s = s1
+			} else {
+				fmt.Println(s)
 				if str == "x" { //checking if user wants to exit or not
 					break
 				} else {
